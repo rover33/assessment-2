c0aa89e514fad50a4658005a2d92f3d53f4b1893
rover33 <remy.j.pearlstone@gmail.com>
2017-12-07 21:45:25 -0700

    yay

--- main.js
+++ main.js
@@ -4,20 +4,23 @@
 // keycode 90 moves #red1 across the screen.
 $(document).keydown(function(run) { 
     if(run.keyCode == 90) { 
-        $("#red1").animate({marginLeft: "+=20vw"}); //animated to make #red1 moved across the screen away from margin left at 20vw.
-       let redRacer = $("#red1").position();
+        $("#red1").animate({position: "+=20"}); //animated to make #red1 moved across the screen away from margin left at 20vw.
+       let redRacer = $("#red1").offset();
         console.log(redRacer.left);
     }
 });
 
 $(document).keydown(function(run) {
     if(run.keyCode == 39) { //runes keycode for right arrow.
-        $("#blue1").animate({marginLeft: "+=20vw"}); //moved #blue1 across the screen at 20vw.
-        let blueRacer = $("#blue1").position();
+        $("#blue1").animate({position: "+=20"}); //moved #blue1 across the screen at 20vw.
+        let blueRacer = $("#blue1").offset();
         console.log(blueRacer.left);
     }
 });
 
-// let winner = function() {
-//     if (redRacer = position.left)
-// }
+
+let winner = ($('.container').width())
+     if ($("#red1") > winner) {
+     
+
+}
