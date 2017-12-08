0fe3b47da3857cbe53285e1d52f8b6b5bdc90755
Remy Pearlstone <remy.j.pearlstone@gmail.com>
2017-12-08 01:23:22 -0700

    street fighter

--- main.js
+++ main.js
@@ -3,20 +3,21 @@
 // used .keyCode to determine which key to use 90(z) and 39(right arrow)
 // keycode 90 moves #red1 across the screen.
 
-// $(document).keydown(function(run) { 
-//     let redRacer = $(window).width() - ($('#red1').offset().left + $('#red1').outerWidth());
-//     if(run.keyCode == 90 && redRacer >= 50) { //runes keycode for right arrow and stops redRacer when he hits the wall
-//         $("#red1").animate({left: '+=2vw'}); 
-//         //animated to make #red1 moved across the screen away from margin left at 2vw.
-//     }
-// });
+$(document).keydown(function(run) { 
+    let redRacer = $(window).width() - ($('#red1').offset().left + $('#red1').outerWidth());
+    if(run.keyCode == 90 && redRacer >= 50) { //runes keycode for right arrow and stops redRacer when he hits the wall
+        $("#red1").animate({left: '+=2vw'}); 
+        console.log(redRacer);
+        //animated to make #red1 moved across the screen away from margin left at 2vw.
+    }
+});
 
-// $(document).keydown(function(run) {
-//     let blueRacer = $(window).width() - ($('#blue1').offset().left + $('#blue1').outerWidth());
-//     if(run.keyCode == 39 && blueRacer >= 50) { //runes keycode for right arrow and stops blueRacer when he hits the wall
-//         $("#blue1").animate({left: '+=2vw'}); //moved #blue1 across the screen at 2vw.
-//     }
-// });
+$(document).keydown(function(run) {
+    let blueRacer = $(window).width() - ($('#blue1').offset().left + $('#blue1').outerWidth());
+    if(run.keyCode == 39 && blueRacer >= 50) { //runes keycode for right arrow and stops blueRacer when he hits the wall
+        $("#blue1").animate({left: '+=2vw'}); //moved #blue1 across the screen at 2vw.
+    }
+});
 
 
 // let winner = $()
@@ -37,10 +38,6 @@
 
 
 
- $(document).keydown(function(run) {
-     if(run.keyCode == 90)
-     $('#red1').animate({left: '+=10'})
- });
 
 
 
